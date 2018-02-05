3ba743c7a2b8e991fb9ce28c10c0975cde6c03a5
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-05 13:45:44 -0500

    Obtiene los valores del formulario, valida que campo de búsqueda no esté vacío

--- js/app.js
+++ js/app.js
@@ -2,3 +2,22 @@
 const eventBrite = new EventBrite(),        // Instancia Clase EventBrite (REST API)
       ui = new Interfaz();                  // Instancia Clase Interfaz (UI)
 
+// Evento 'click' al Botón de búsqueda del formulario
+document .getElementById( 'buscarBtn' ) .addEventListener( 'click', e => {
+    const nombreCiudad = document .getElementById( 'evento' ) .value,                                   // Obtiene el valor del elemento 'input' (Buscar por Nombre o Ciudad)
+          selectCategoria = document .getElementById( 'listado-categorias' ),                           // Obtiene el elemento 'select' (Categoría Evento)
+          categoriaSeleccionada = selectCategoria .options[ selectCategoria .selectedIndex ] .value;    // Obtiene la categoria seleccionada en el 'select'
+
+    e .preventDefault();        // Previene la ejecución del 'action' o acciones por defecto del formulario
+
+    console .log( 'Nombre o Ciudad ' + nombreCiudad );
+    console .log( 'Categoría Evento ' + categoriaSeleccionada );
+
+    // Valida que el campo del buscador no esté vacío (Nombre o Ciudad)
+    if( nombreCiudad !== '' ) {
+        console .log( 'Buscando...' );
+    }
+    else {
+        console .log( 'Hey! no hay nada en el campo de búsqueda' );
+    }
+});
